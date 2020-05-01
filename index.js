0d5ab2d2dc74001cb0fe72fd7060e5eeaa9941b0
Joseph Haaga <haaga.joe@gmail.com>
2020-05-01 17:06:47 -0400

    clean up

---
+++ index.js
@@ -0,0 +1,35 @@
+(function() {
+  var fs = require("fs");
+  var path = require("path");
+  var Handlebars = require("handlebars");
+  var handlebarsHelpers = require("./template/HandlebarHelpers");
+  var phantomjs = require('phantomjs-prebuilt');
+
+  var base = require("./resume.json");
+
+  var compiled = __dirname + '/compiled';
+  if (!fs.existsSync(compiled)) {
+    fs.mkdirSync(compiled, 0744);
+  }
+  handlebarsHelpers.register();
+
+  function render(resume, filename) {
+    var css = fs.readFileSync(__dirname + "/template/style.css", "utf-8");
+    var tpl = fs.readFileSync(__dirname + "/template/resume.hbs", "utf-8");
+
+    //generates HTML
+    fs.writeFile(compiled + "/" + filename + ".html", Handlebars.compile(tpl)({
+      css: css,
+      resume: resume
+    }), {
+      flag: 'w'
+    }, function(err, written, buffer) {});
+
+    // generates PDF
+    var program = phantomjs.exec('./scripts/rasterize.js', './compiled/' + filename + '.html', './compiled/' + filename + '.pdf', 'Letter')
+    program.stdout.pipe(process.stdout);
+    program.stderr.pipe(process.stderr);
+  }
+
+  render(base, 'JosephHaaga.en');
+}());
